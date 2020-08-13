c3b4573099a4729d699af8d2acc0d946336fd8a5
BE1NG <ht8981@gmail.com>
2020-08-13 10:14:23 +0900

    합본

--- space/models.py
+++ space/models.py
@@ -5,9 +5,7 @@ class User(models.Model):
     email = models.CharField(primary_key=True, max_length=50)
     password = models.CharField(max_length=100)
     name = models.CharField(max_length=10)
-<<<<<<< HEAD
     phone = models.CharField(max_length=15)
-=======
 
 # 게시글 DB
 class Posting(models.Model):
@@ -15,4 +13,3 @@ class Posting(models.Model):
     content = models.CharField(max_length=1000)
     picture = models.CharField(max_length=500)
     location = models.CharField(max_length=500)
->>>>>>> 35bbd1a18bbecc1e44d0bbcfccde22673076ff52
